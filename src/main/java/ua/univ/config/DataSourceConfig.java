f7375dfd309a626ba4b168a8983498c2f8325c26
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-05-12 11:51:56 +0300

    Adding all necessary dependencies

---
+++ src/main/java/ua/univ/config/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package ua.univ.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSourceConfig {
+    /*  Database credentials */
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/FacultyOOPLabs";
+    static final String USER = "postgres";
+    static final String PASS = "password";
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            e.printStackTrace();
+            return null;
+        }
+
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+}
