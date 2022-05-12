179ca531c5872d49ad4f2eb547762134de7ee2d3
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-05-12 12:09:06 +0300

    Add filters to prevent xss attack

---
+++ src/main/java/ua/univ/filters/XssFilter.java
@@ -0,0 +1,24 @@
+package ua.univ.filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class XssFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // Using wrappers
+        XSSRequestWrapper XSSRequestWrapper=new XSSRequestWrapper((HttpServletRequest) servletRequest);
+        filterChain.doFilter(XSSRequestWrapper,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
