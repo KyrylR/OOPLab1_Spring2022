1fe7e68ba985dc918765b577f852c75946464f6c
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-06-07 11:58:07 +0300

    Finish

--- src/main/java/ua/univ/filters/KeycloakFilter.java
+++ src/main/java/ua/univ/filters/KeycloakFilter.java
@@ -19,36 +19,43 @@ import java.util.Set;
 public class KeycloakFilter implements Filter {
     private final List<String> requiredRoles = Arrays.asList("ROLE_ADMIN", "ROLE_MANAGER");
 
+    private void CorsOptions(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS,  DELETE");
+        response.addHeader("Access-Control-Allow-Headers", "*");
+    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        boolean hasRequiredRole = false;
+        this.CorsOptions(response);
 
         if (request.getMethod().equals("OPTIONS")) {
+            return;
+        }
+
+        boolean hasRequiredRole = false;
+        try {
+            AccessToken accessToken = KeycloakTokenUtil.getToken(request, request.getHeader("Authorization"));
+            Set<String> roles = KeycloakTokenUtil.getRoles(Objects.requireNonNull(accessToken));
+            for (String item : roles) {
+                if (requiredRoles.contains(item)) {
+                    hasRequiredRole = true;
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+            response.setStatus(401);
+            return;
+        }
+
+        if (hasRequiredRole) {
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
-            try {
-                AccessToken accessToken = KeycloakTokenUtil.getToken(request, request.getHeader("Authorization"));
-                Set<String> roles = KeycloakTokenUtil.getRoles(Objects.requireNonNull(accessToken));
-                for (String item: roles) {
-                    if (requiredRoles.contains(item)) {
-                        hasRequiredRole = true;
-                        break;
-                    }
-                }
-            } catch (Exception ex) {
-                log.error(ex.getMessage());
-                response.setStatus(401);
-                return;
-            }
-
-            if (hasRequiredRole) {
-                filterChain.doFilter(servletRequest, servletResponse);
-            } else {
-                response.setStatus(403);
-            }
+            response.setStatus(403);
         }
     }
-
 }
+
